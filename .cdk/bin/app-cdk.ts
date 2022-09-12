56b8f8334cb8e70f1e26324360cae93c0b4266b5
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2022-09-12 14:37:50 +1000

    first commit

---
+++ .cdk/bin/app-cdk.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib'
+import { EcsDeployStack } from '../lib/ecs-deploy-stack'
+import { toCamel, capitalizeFirstLetter } from '../helpers/string'
+import environmentConfig from '../config'
+
+const stage = process.env.STAGE as string
+const appName = capitalizeFirstLetter(toCamel(environmentConfig.name))
+const appRevision = process.env.STAGE === 'preview' ? process.env.APP_REVISION : ''
+const name = `${appName}${capitalizeFirstLetter(stage)}${appRevision}`
+const stackName = toCamel(`Ecs-App-${name}-Stack`)
+
+const stageConfig = environmentConfig.stages[stage]
+
+const app = new cdk.App()
+
+new EcsDeployStack(app, `EcsDeployStack-${stage}`, {
+  env: {
+    account: stageConfig.aws.account,
+    region: stageConfig.aws.region,
+  },
+  stage,
+  stackName,
+  appName,
+  clusterArn: stageConfig.ecs.cluster_arn,
+  route53: {
+    ...stageConfig.route53,
+    hostname: process.env.STAGE === 'preview' ? `${stageConfig.route53.hostname}-${appRevision}` : stageConfig.route53.hostname,
+  },
+  secretsManager: stageConfig.secretsManager,
+  acm: stageConfig.acm,
+  vpc: {
+    ...stageConfig.vpc,
+    name: stageConfig.vpc?.name || `${stageConfig.aws.account_name}-VPC`,
+  },
+  container: {
+    ...environmentConfig.container,
+    image: {
+      ...environmentConfig.container.image,
+      version: process.env.APP_REVISION,
+    },
+  },
+  task: stageConfig.task,
+  loadBalancer: stageConfig.loadBalancer,
+  autoscaling: stageConfig.autoscaling,
+})
+
+app.synth()
---
+++ .cdk/config/index.ts
@@ -0,0 +1,92 @@
+const configGLobal = require('../cdk.config.json')
+
+interface IAws {
+  readonly account_name: string
+  readonly account: string
+  readonly region: string
+}
+
+export interface IVpc {
+  readonly name?: string
+  readonly id?: string
+}
+
+export interface IRoute53 {
+  readonly enable: boolean
+  readonly domain: string
+  readonly hostname: string
+}
+
+export interface IAcm {
+  readonly create: boolean
+  readonly arn: string
+}
+
+export interface ISecretsManager {
+  readonly arn: string
+  readonly variables: {
+    [key: string]: string
+  }
+}
+
+export interface IContainer {
+  readonly port: number
+  readonly image: {
+    readonly uri: string
+    readonly version?: string
+  }
+  readonly buildArgs: {
+    [key: string]: string
+  }
+}
+
+export interface ITask {
+  readonly desiredCount: number
+  readonly cpu: number
+  readonly memoryLimitMiB: number
+  readonly spot: boolean
+}
+
+export interface IAutoscaling {
+  readonly minCapacity: number
+  readonly maxCapacity: number
+  readonly cpuTargetUtilizationPercent: number
+}
+
+export interface ILoadBalancer {
+  readonly healthcheckPath: string
+}
+
+export interface ICloudWatchAlarm {
+  readonly alarmThreshold: number
+  readonly evaluationPeriods: number
+  readonly datapointsToAlarm?: number
+}
+
+export interface IEcs {
+  readonly cluster_arn?: string
+}
+
+export interface IStages {
+  [key: string]: {
+    aws: IAws
+    vpc?: IVpc
+    route53: IRoute53
+    acm: IAcm
+    secretsManager: ISecretsManager[]
+    task: ITask
+    autoscaling?: IAutoscaling
+    loadBalancer?: ILoadBalancer
+    ecs: IEcs
+  }
+}
+
+export interface IEnvironmentConfig {
+  readonly name: string
+  readonly container: IContainer
+  readonly stages: IStages
+}
+
+const Environment: IEnvironmentConfig = configGLobal
+
+export default Environment
